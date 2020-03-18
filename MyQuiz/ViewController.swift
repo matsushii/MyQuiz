e12e7c45bd390e34f124d3ac2ecb34c034eb4808
matsushii <mtsmath24@yahoo.co.jp>
2020-03-18 23:10:24 +0900

    Initial Commit

---
+++ MyQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyQuiz
+//
+//  Created by M.Atsuhi on 2020/03/18.
+//  Copyright © 2020 M.Atsuhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
